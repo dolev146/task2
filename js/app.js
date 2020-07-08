ef46869538f4cf4df2d18726b448f2f46df66be2
dolev146 <62290677+dolev146@users.noreply.github.com>
2020-07-08 12:46:17 +0300

    almost
    almost

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import getNorthwind from "./Ajax.js";
-let all = document.querySelector('#all');
+let input = document.querySelector("#input");
+let all = document.querySelector("#all");
 all.addEventListener("click", () => {
     getNorthwind("https://restcountries.eu/rest/v2/all?fields=name;topLevelDomain;capital;currencies;borders;flag", firstLoad);
 });
@@ -21,5 +22,35 @@ function firstLoad() {
     if (list) {
         list.innerHTML = buttons.join("");
     }
+    input.focus();
+}
+let send = document.querySelector("#send");
+send.addEventListener("click", () => {
+    let inputvalue = document.querySelector("#input").value;
+    if (inputvalue === "") {
+        throw alert("you didnt inserted value");
+    }
+    getNorthwind(`https://restcountries.eu/rest/v2/name/${inputvalue}?fields=name;topLevelDomain;capital;currencies;borders;flag
+    `, search);
+});
+function search() {
+    let customers = JSON.parse(this.responseText);
+    // console.log(customers)
+    let buttons = customers.map((customer) => `<div style="display:flex; margin-bottom: 1em;"> 
+                          <img src="${customer.flag}" alt="couty flag" width="50%" style="margin-right: 1em;" >
+                          <div style=" display: inline-block;   width:40%";>
+                          name: <p>${customer.name}</p> <br>
+                          capital: <p>${customer.capital}</p> <br>
+                          currencies: <p><br> code: ${customer.currencies[0].code} <br> currencies name: ${customer.currencies[0].name} <br> symbol: ${customer.currencies[0].symbol} </p> <br>
+                          borders: <p>${customer.borders}</p> <br>
+                          Top level domain : <p>${customer.topLevelDomain}</p><br>
+                          </div>
+                      </div>
+                      `);
+    let list = document.querySelector("#note");
+    if (list) {
+        list.innerHTML = buttons.join("");
+    }
+    input.focus();
 }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import getNorthwind from "./Ajax.js";
 import { Customer } from "./customer.model.js";
-let all :Element | null = document.querySelector('#all')
+let input: Element | null = document.querySelector("#input");
+let all: Element | null = document.querySelector("#all");
+
 all.addEventListener("click", () => {
   getNorthwind(
     "https://restcountries.eu/rest/v2/all?fields=name;topLevelDomain;capital;currencies;borders;flag",
@@ -11,7 +13,7 @@ all.addEventListener("click", () => {
 function firstLoad(): void {
   let customers: Customer[] = JSON.parse(this.responseText);
   // console.log(customers)
-  
+
   let buttons = customers.map(
     (customer) => `<div style="display:flex; margin-bottom: 1em;"> 
                         <img src="${customer.flag}" alt="couty flag" width="50%" style="margin-right: 1em;" >
@@ -29,4 +31,43 @@ function firstLoad(): void {
   if (list) {
     list.innerHTML = buttons.join("");
   }
+  input.focus();
+}
+
+let send = document.querySelector("#send");
+
+send.addEventListener("click", () => {
+  let inputvalue: any = document.querySelector("#input").value;
+  if (inputvalue === "") {
+    throw alert("you didnt inserted value")
+  }
+  getNorthwind(
+    `https://restcountries.eu/rest/v2/name/${inputvalue}?fields=name;topLevelDomain;capital;currencies;borders;flag
+    `,
+    search
+  );
+});
+
+function search(): void {
+  let customers: Customer[] = JSON.parse(this.responseText);
+  // console.log(customers)
+
+  let buttons = customers.map(
+    (customer) => `<div style="display:flex; margin-bottom: 1em;"> 
+                          <img src="${customer.flag}" alt="couty flag" width="50%" style="margin-right: 1em;" >
+                          <div style=" display: inline-block;   width:40%";>
+                          name: <p>${customer.name}</p> <br>
+                          capital: <p>${customer.capital}</p> <br>
+                          currencies: <p><br> code: ${customer.currencies[0].code} <br> currencies name: ${customer.currencies[0].name} <br> symbol: ${customer.currencies[0].symbol} </p> <br>
+                          borders: <p>${customer.borders}</p> <br>
+                          Top level domain : <p>${customer.topLevelDomain}</p><br>
+                          </div>
+                      </div>
+                      `
+  );
+  let list: Element | null = document.querySelector("#note");
+  if (list) {
+    list.innerHTML = buttons.join("");
+  }
+  input.focus();
 }
